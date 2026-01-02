a6a48c1af5e5df68903c1f4d307d680593f5f272
Munnoi <midhunbinoy123@gmail.com>
2026-01-02 13:16:43 +0530

    Added urls, and html for home page

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def view_posts(request):
+    posts = Post.objects.all()
+    return render(request, "posts/index.html", {"posts": posts})
+
+
+def view_post(request):
+    pass
+
+
+def create_post(request):
+    pass
+
+
+def edit_post(request):
+    pass
+
+
+def delete_post(request):
+    pass
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
+    path("posts/", include("apps.posts.urls"))
 ]
